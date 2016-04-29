ebe77c42f9c5dfd5caec7757902d797b15b473d0
rosso0117 <rollwithit0117@gmail.com>
2016-04-29 18:31:41 +0900

    refactored code

--- colors/ViewController.swift
+++ colors/ViewController.swift
@@ -9,155 +9,132 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private var colorView:UIView!
+    let rgbaCount = 4
+    let rgbaTitles = ["Red", "Green", "Blue", "Alpha"]
+    let barHeight = UIApplication.sharedApplication().statusBarFrame.size.height
+    let initialValueOfSlider:Float = 0.5
     
-    private var redView:UIView!
-    private var greenView:UIView!
-    private var blueView:UIView!
-    private var alphaView:UIView!
-    private var rgbaView:[UIView]!
+    var colorView:UIView!
+    var colorViewRGBAValues = [CGFloat]()
     
-    private var redLabel:UILabel!
-    private var greenLabel:UILabel!
-    private var blueLabel:UILabel!
-    private var alphaLabel:UILabel!
-    private var rgbaLabel:[UILabel]!
-    
-    private var redSlider:UISlider!
-    private var greenSlider:UISlider!
-    private var blueSlider:UISlider!
-    private var alphaSlider:UISlider!
-    private var rgbaSlider:[UISlider]!
+    var rgbaViewArray = [UIView]()
+    var rgbaLabelArray = [UILabel]()
+    var rgbaSliderArray = [UISlider]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        colorViewRGBAValues = Array(count: rgbaCount, repeatedValue: CGFloat(initialValueOfSlider))
         //colorView
         colorView = UIView(frame: CGRectMake(0, 0, 180, 180))
-        colorView.layer.position = CGPoint(x:self.view.bounds.width/2, y:self.view.frame.height/4 + 20)
+        colorView.layer.position = CGPoint(x:self.view.bounds.width/2,
+                                           y:self.view.frame.height/4 + barHeight)
         colorView.layer.masksToBounds = true
         colorView.layer.cornerRadius = 90.0
+        colorView.backgroundColor = UIColor(red: colorViewRGBAValues[0],
+                                            green: colorViewRGBAValues[1],
+                                            blue: colorViewRGBAValues[2],
+                                            alpha: colorViewRGBAValues[3])
         self.view.addSubview(colorView)
         
         //rgbaView
-        
-        redView = UIView()
-        greenView = UIView()
-        blueView = UIView()
-        alphaView = UIView()
-        rgbaView = [redView,greenView,blueView,alphaView]
-        
-        
-        for (index, rgbaV) in rgbaView.enumerate(){
+        for index in 0 ..< rgbaCount {
+            let myView = UIView()
             let yPos = CGFloat(index)
-            let xyPos = CGPoint(x: self.view.bounds.width * 4/10, y: self.view.bounds.height * (7 + yPos) / 12)
-            rgbaV.frame = CGRect(x:0, y:0, width:26, height:26)
-            rgbaV.layer.position = xyPos
-            rgbaV.layer.masksToBounds = true
-            rgbaV.layer.cornerRadius = 13.0
-            self.view.addSubview(rgbaV)
+            let xyPos = CGPoint(x: self.view.bounds.width * 4/10,
+                y: self.view.bounds.height * (7 + yPos) / 12)
+            myView.frame = CGRect(x:0, y:0, width:26, height:26)
+            myView.layer.position = xyPos
+            myView.layer.masksToBounds = true
+            myView.layer.cornerRadius = 13.0
+            myView.tag = index
+            myView.backgroundColor = setEachViewColor(myView.tag)
+            rgbaViewArray.append(myView)
+            self.view.addSubview(myView)
         }
-        
         
         //rgbaLabel
-        
-        redLabel = UILabel()
-        redLabel.text = "Red"
-        
-        greenLabel = UILabel()
-        greenLabel.text = "Green"
-        
-        blueLabel = UILabel()
-        blueLabel.text = "Blue"
-        
-        alphaLabel = UILabel()
-        alphaLabel.text = "Alpha"
-        
-        rgbaLabel = [redLabel, greenLabel, blueLabel, alphaLabel]
-        for(index, label) in rgbaLabel.enumerate() {
+        for index in 0 ..< rgbaCount {
+            let myLabel = UILabel()
             let yPos = CGFloat(index)
-            let xyPos = CGPoint(x: self.view.bounds.width/8, y: self.view.bounds.height * (7 + yPos) / 12)
-            label.frame = CGRectMake(0, 0, self.view.bounds.width/4, 26)
-            label.layer.position = xyPos
-            label.textAlignment = .Right
-            self.view.addSubview(label)
+            let xyPos = CGPoint(x: self.view.bounds.width/8,
+                y: self.view.bounds.height * (7 + yPos) / 12)
+            myLabel.frame = CGRectMake(0, 0, self.view.bounds.width/4, 26)
+            myLabel.layer.position = xyPos
+            myLabel.textAlignment = .Right
+            myLabel.tag = index
+            rgbaViewArray.append(myLabel)
+            myLabel.text = rgbaTitles[index]
+            self.view.addSubview(myLabel)
         }
-        
+
         //rgbaSlider
-        
-        redSlider = UISlider()
-        greenSlider = UISlider()
-        blueSlider = UISlider()
-        alphaSlider = UISlider()
-        
-        rgbaSlider = [redSlider, greenSlider, blueSlider, alphaSlider]
-        
-        for (index, slider) in rgbaSlider.enumerate() {
+        for index in 0 ..< rgbaCount {
+            let mySlider = UISlider()
             let yPos = CGFloat(index)
             let xyPos = CGPoint(x: self.view.bounds.width * 7/10, y: self.view.bounds.height * (7 + yPos) / 12)
-            slider.frame = CGRectMake(0, 0, self.view.bounds.width/3, 20)
-            slider.layer.position = xyPos
-            slider.minimumValue = 0.0
-            slider.maximumValue = 1.0
-            slider.value = 1.0
-            slider.addTarget(self, action: #selector(rgbaChange(_:)), forControlEvents: .ValueChanged)
-            slider.addTarget(self, action: #selector(colorChange(_:)), forControlEvents: .ValueChanged)
-            self.view.addSubview(slider)
+            mySlider.frame = CGRectMake(0, 0, self.view.bounds.width/3, 20)
+            mySlider.layer.position = xyPos
+            mySlider.minimumValue = 0.0
+            mySlider.maximumValue = 1.0
+            mySlider.value = initialValueOfSlider
+            mySlider.addTarget(self, action: #selector(valueChanged(_:)), forControlEvents: .ValueChanged)
+            mySlider.tag = index
+            rgbaSliderArray.append(mySlider)
+            self.view.addSubview(mySlider)
         }
-        
     }
     
     
-    func rgbaChange(slider: UISlider) -> CGFloat{
+    func valueChanged(slider: UISlider){
         let changedValue = CGFloat(slider.value)
-        var rChanged, gChanged, bChanged, aChanged:CGFloat
         
-        
-        switch slider {
-        case redSlider:
-            redView.backgroundColor = UIColor(red: changedValue, green: 0.0, blue: 0.0, alpha: 1.0)
-            rChanged = changedValue
-            return rChanged
-        case greenSlider:
-            greenView.backgroundColor = UIColor(red: 0.0, green: changedValue, blue: 0.0, alpha: 1.0)
-            gChanged = changedValue
-            return gChanged
-        case blueSlider:
-            blueView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: changedValue, alpha: 1.0)
-            bChanged = changedValue
-            return bChanged
-        case alphaSlider:
-            alphaView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: changedValue)
-            aChanged = changedValue
-            return aChanged
+        switch slider.tag {
+        case 0:
+            rgbaViewArray[0].backgroundColor = UIColor(red: changedValue, green: 0.0,
+                                              blue: 0.0, alpha: 1.0)
+            colorViewRGBAValues[0] = changedValue
+        case 1:
+            rgbaViewArray[1].backgroundColor = UIColor(red: 0.0, green: changedValue,
+                                                blue: 0.0, alpha: 1.0)
+            colorViewRGBAValues[1] = changedValue
+        case 2:
+            rgbaViewArray[2].backgroundColor = UIColor(red: 0.0, green: 0.0,
+                                               blue: changedValue, alpha: 1.0)
+            colorViewRGBAValues[2] = changedValue
+        case 3:
+            rgbaViewArray[3].backgroundColor = UIColor(red: 0.0, green: 0.0,
+                                                blue: 0.0, alpha: changedValue)
+            colorViewRGBAValues[3] = changedValue
         default:
-            return changedValue
+            break
         }
         
+        colorView.backgroundColor = UIColor(red: colorViewRGBAValues[0],
+                                            green: colorViewRGBAValues[1],
+                                            blue: colorViewRGBAValues[2],
+                                            alpha: colorViewRGBAValues[3])
     }
     
-    
-    func colorChange(slider:UISlider){
-        let changedValue = CGFloat(slider.value)
-        let rChanged = rgbaChange(redSlider)
-        let gChanged = rgbaChange(greenSlider)
-        let bChanged = rgbaChange(blueSlider)
-        let aChanged = rgbaChange(alphaSlider)
-        switch slider {
-        case redSlider:
-            colorView.backgroundColor = UIColor(red: changedValue, green: gChanged, blue: bChanged, alpha: aChanged)
-        case greenSlider:
-            colorView.backgroundColor = UIColor(red: rChanged, green: changedValue, blue: bChanged, alpha: aChanged)
-        case blueSlider:
-            colorView.backgroundColor = UIColor(red: rChanged, green: gChanged, blue: changedValue, alpha: aChanged)
-        case alphaSlider:
-            colorView.backgroundColor = UIColor(red: rChanged, green: gChanged, blue: bChanged, alpha: changedValue)
+    func setEachViewColor(tag: Int) -> UIColor {
+        let initialEachValue = CGFloat(initialValueOfSlider)
+        switch tag {
+        case 0:
+            return UIColor(red: initialEachValue, green: 0.0,
+                           blue: 0.0, alpha: 1.0)
+        case 1:
+            return UIColor(red: 0.0, green: initialEachValue,
+                           blue: 0.0, alpha: 1.0)
+        case 2:
+            return UIColor(red: 0.0, green: 0.0,
+                           blue: initialEachValue, alpha: 1.0)
+        case 3:
+            return UIColor(red: 0.0, green: 0.0,
+                           blue: 0.0, alpha: initialEachValue)
         default:
-            break;
+            return UIColor.blackColor()
         }
     }
-    
     
     
     
